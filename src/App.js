aeaee4a673d09278950100d6b4147b4b16b0c0fc
Matus Kello <m.kello1992@gmail.com>
2023-08-30 19:32:54 +0200

    +MUI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import allMovies from './data';
 import { useState, useEffect } from 'react';
+import {
+  Card,
+  Box,
+  Typography,
+  ThemeProvider,
+  TextField,
+  ImageList,
+} from '@mui/material';
+import theme from './config/theme';
 
 const App = () => {
   const [searchingText, setSearchingText] = useState('');
@@ -13,29 +22,63 @@ const App = () => {
   }, [searchingText]);
 
   return (
-    <div>
-      <form action=''>
-        <input
+    <ThemeProvider theme={theme}>
+      <Box sx={{ background: theme.palette.primary.main }}>
+        <TextField
+          variant='filled'
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: '1rem',
+            marginTop: '0',
+            background: theme.palette.tertiary.main,
+          }}
           type='text'
           placeholder='Search'
           onChange={(e) => setSearchingText(e.target.value)}
         />
-      </form>
-      <div>
-        {filteredMovies.map((oneMovie) => {
-          const { id, image, title, age, tags, description } = oneMovie;
-          return (
-            <div key={id}>
-              <img src={image} alt='' />
-              <h2>{title}</h2>
-              <p>{age}</p>
-              <p>{tags}</p>
-              <p>{description}</p>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+        <Card
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+            flexDirection: 'row',
+            paddingBottom: '10rem',
+            background: theme.palette.primary.main,
+          }}
+        >
+          {filteredMovies.map((oneMovie) => {
+            const { id, image, title, age, tags, description } = oneMovie;
+
+            return (
+              <Box
+                key={id}
+                sx={{
+                  width: '50rem',
+                  margin: '1rem',
+                  padding: '1rem',
+                  boxShadow: '0px 0px 10px #e50914',
+                  color: theme.palette.tertiary.main,
+                }}
+              >
+                <ImageList
+                  sx={{
+                    width: '20rem',
+                  }}
+                >
+                  <img src={image} alt='' />
+                </ImageList>
+                <Typography variant='h5'>{title}</Typography>
+                <Typography>{age}</Typography>
+                <Typography>{tags}</Typography>
+                <Typography variant='subtitle1'>{description}</Typography>
+              </Box>
+            );
+          })}
+        </Card>
+      </Box>
+    </ThemeProvider>
   );
 };
 
---
+++ src/config/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#141414',
+    },
+    secondary: {
+      main: '#e50914',
+    },
+    tertiary: {
+      main: '#e5e5e5',
+    },
+  },
+});
+
+export default theme;
